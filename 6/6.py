9f8f9edfcc9d9171697a7906775156f7f2d31e1b
Martin Sturm <martin.sturm@adevinta.com>
2022-12-06 10:40:44 +0100

    Day 6

---
+++ 6/6.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from collections import deque
+file1 = '6.in'
+
+input = [x.strip() for x in open(file1, 'r').readlines()][0]
+# input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+recent = deque()
+recent2 = deque()
+ans1 = 0
+ans2 = 0
+start = 0
+n = 0
+while n < len(input):
+    x = input[n]
+    print(n, x)
+    if x in recent2:
+        start = start + 1
+        n = start
+        recent2.clear()
+    else:
+        n += 1
+        recent2.append(x)
+
+    if len(recent2) == 4 and ans1 == 0:
+        ans1 = n
+        break
+
+start = 0
+n = 0
+while n < len(input):
+    x = input[n]
+    print(n, x)
+    if x in recent2:
+        start = start + 1
+        n = start
+        recent2.clear()
+    else:
+        n += 1
+        recent2.append(x)
+
+    if len(recent2) == 14 and ans2 == 0:
+        ans2 = n
+        break
+
+print(ans1)
+print(ans2)
