2382d5ae8111aa487021d272a15eda1a779712d8
Martin Sturm <martin@martinsturm.nl>
2022-12-18 21:37:03 +0100

    Fix day 12

--- 12/12.py
+++ 12/12.py
@@ -1,39 +1,49 @@
 #!/usr/bin/env python3
-file1 = '12.test'
+file1 = '12.in'
+from collections import deque
+input = [x.strip() for x in open(file1, 'r').readlines()]
 G = {}
 start = (0,0)
 end = (0,0)
+max_r = len(input)
+max_c = len(input[0])
 
-input = [x.strip() for x in open(file1, 'r').readlines()]
 for r in range(len(input)):
     for c in range(len(input[r])):
         if input[r][c] == 'S':
             start = (r, c)
-            G[(r,c)] = 0
+            G[(r,c)] = ord('a') - 97
         elif input[r][c] == 'E':
             end = (r, c)
-            G[(r,c)] = 26
+            G[(r,c)] = ord('z') - 97
         else:
             G[(r, c)] = ord(input[r][c]) - 97
-path = [start]
-D = {(0, 1), (1, 0), (-1, 0), (0, -1)}
-while path[-1] != end:
-    cur = path[-1]
-    if cur[0] < end[0] and G[(cur[0]+1,cur[1])] <= (G[(cur[0],cur[1])] + 1) and (cur[0]+1,cur[1]) not in path:
-        path.append((cur[0] + 1, cur[1]))
-    elif cur[1] < end[1] and G[(cur[0],cur[1]+1)] <= (G[(cur[0],cur[1])] + 1) and (cur[0],cur[1]+1) not in path:
-        path.append((cur[0], cur[1] + 1))
-    elif cur[0] > end[0] and G[(cur[0] - 1,cur[1])] <= (G[(cur[0],cur[1])] + 1) and (cur[0] - 1,cur[1]) not in path:
-        path.append((cur[0] - 1, cur[1]))
-    elif cur[1] > end[1] and G[(cur[0],cur[1]-1)] <= (G[(cur[0],cur[1])] + 1) and (cur[0],cur[1]-1) not in path:
-        path.append((cur[0], cur[1] - 1)) 
-    elif cur[0] >= end[0] and G[(cur[0]+1,cur[1])] <= (G[(cur[0],cur[1])] + 1) and (cur[0]+1,cur[1]) not in path:
-        path.append((cur[0] + 1, cur[1]))
-    elif cur[1] >= end[1] and G[(cur[0],cur[1]+1)] <= (G[(cur[0],cur[1])] + 1) and (cur[0],cur[1]+1) not in path:
-        path.append((cur[0], cur[1] + 1))
-    elif cur[0] >= end[0] and G[(cur[0] - 1,cur[1])] <= (G[(cur[0],cur[1])] + 1) and (cur[0] - 1,cur[1]) not in path:
-        path.append((cur[0] - 1, cur[1]))
-    elif cur[1] >= end[1] and G[(cur[0],cur[1]-1)] <= (G[(cur[0],cur[1])] + 1) and (cur[0],cur[1]-1) not in path:
-        path.append((cur[0], cur[1] - 1)) 
-    print(path)
-print(start, end, G)
+
+not_found = 0
+
+def find_path(sr, sc):
+    global not_found
+    # print(sr, sc)
+    D = {(0, 1), (1, 0), (-1, 0), (0, -1)}
+    todo = deque()
+    todo.append((sr, sc, 0))
+    S = {(0, 0): 0}
+    
+    while len(todo) > 0:
+        r, c, d = todo.popleft()
+        for dr, dc in D:
+            rr = r + dr
+            cc = c + dc
+            if 0 <= rr < max_r and 0 <= cc < max_c and ((G[(rr, cc)] <= G[(r, c)] + 1)):
+                # print("here")
+                if (rr, cc) not in S:
+                    # print("not in S", cd_point, S)
+                    S[(rr, cc)] = d + 1
+                    todo.append((rr, cc, d + 1))
+    if end not in S:
+        not_found += 1
+        return 9999
+    return S[end]
+
+print("part 1", find_path(start[0], start[1]))
+print("part 2", min([x for x in [find_path(sr, sc) for sr, sc in G if G[(sr, sc)] == ord('a') - 97]]))
