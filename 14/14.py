123451b733f3b1312b564abdc90697e8c77924e1
Martin Sturm <martin@martinsturm.nl>
2022-12-19 18:24:34 +0100

    Day 15

--- 14/14.py
+++ 14/14.py
@@ -1,5 +1,68 @@
 #!/usr/bin/env python3
+from copy import deepcopy
 file1 = '14.in'
 
+G = {}
+max_y = 0
 input = [x.strip() for x in open(file1, 'r').readlines()]
 for v in input:
+    D = [(int(u), int(v)) for u, v in [y.split(',') for y  in [x.strip() for x in v.split('->')]]]
+    E = [(p1, p2) for p1, p2 in zip(D[0:-1], D[1:])]
+    for p1, p2 in E:
+        xmin = p1[0] if p1[0] <= p2[0] else p2[0]
+        xmax = p2[0] if p1[0] <= p2[0] else p1[0]
+        ymin = p1[1] if p1[1] <= p2[1] else p2[1]
+        ymax = p2[1] if p1[1] <= p2[1] else p1[1]
+
+        for x1 in range(xmin, xmax + 1):
+            for y1 in range(ymin, ymax + 1):
+                G[(x1, y1)] = '#'
+        max_y = max(max_y, ymax)
+print(sorted(G))
+
+G1 = deepcopy(G)
+unstable = True
+s = (500, 0)
+i = 0
+while s[1] < max_y:
+    sx, sy = s
+    if (sx, sy + 1) not in G1:
+        s = (sx, sy + 1)
+    elif (sx - 1, sy + 1) not in G1:
+        s = (sx - 1, sy + 1)
+    elif (sx + 1, sy + 1) not in G1:
+        s = (sx + 1, sy + 1)
+    else:
+        G1[(sx, sy)] = 'o'
+        s = (500, 0)
+        i += 1
+    
+print("p1", i)
+
+# part 2
+
+G2 = deepcopy(G)
+unstable = True
+s = (500, 0)
+blocked = False
+i = 0
+while not blocked:
+    sx, sy = s
+    if sy == max_y + 1: # infinte floor
+        G2[(sx, sy)] = 'o'
+        s = (500, 0)
+        i += 1
+    elif (sx, sy + 1) not in G2:
+        s = (sx, sy + 1)
+    elif (sx - 1, sy + 1) not in G2:
+        s = (sx - 1, sy + 1)
+    elif (sx + 1, sy + 1) not in G2:
+        s = (sx + 1, sy + 1)
+    else:
+        G2[(sx, sy)] = 'o'
+        i += 1
+        if (sx, sy) == (500, 0): # blocked
+            break
+        s = (500, 0)
+print("p2", i)
+        
--- 15/15.py
+++ 15/15.py
@@ -1,5 +1,70 @@
 #!/usr/bin/env python3
+# file1 = '15.test'
 file1 = '15.in'
-
+from copy import deepcopy
 input = [x.strip() for x in open(file1, 'r').readlines()]
+G = set()
+
+B = set()
+max_x = 0
+min_x = 0
 for v in input:
+    words = v.split()
+    sx = words[2].split('=')[1][0:-1]
+    sy = words[3].split('=')[1][0:-1]
+    bx = words[-2].split('=')[1][0:-1]
+    by = words[-1].split('=')[1]
+    bx, by, sx, sy = int(bx), int(by), int(sx), int(sy)
+    G.add((sx, sy, abs(bx - sx) + abs(by - sy)))
+    B.add((bx, by))
+    max_x = max(max_x, (sx + abs(bx - sx) + abs(by - sy)))
+    min_x = min(min_x, (sx - abs(bx - sx) + abs(by - sy)))
+    # print(bx, by, sx, sy)
+    # print(G)
+
+
+# [print(x, G[x]) for x in G]
+# y = 1
+y = 2000000
+blocked = set()
+for (sx, sy, d) in G:
+    if d >= abs(y-sy):
+        r = d - abs(y-sy)
+        for xx in range(-r, r+1): 
+            if (sx+xx, y) not in B:
+                blocked.add((sx + xx, y))
+
+print("p1", len(blocked))
+# max_range = 20
+max_range = 4000000
+
+def valid(x, y, G):
+    if not (0 <= x <= max_range and 0 <= y <= max_range):
+        return False
+    for sx, sy, d in G:
+        dxy = abs(sx - x) + abs(sy - y)
+        if d >= dxy:
+            return False
+    return True
+
+points_to_check = set()
+counter = 0
+p2 = 0
+for (sx, sy, d) in G:
+    mhd = d + 1
+    for dd in range(0, mhd):
+        new_points = [(sx + mhd - dd, sy + dd),
+            (sx - mhd + dd, sy + dd),
+            (sx + dd, sy + mhd - dd),
+            (sx + dd, sy - mhd + dd)]
+        for np in new_points:
+            counter += 1
+            if counter % 500000 == 0:
+                print('.', end='')
+            np_x, np_y = np
+            if valid(np_x, np_y, G):
+                p2 = np_x * 4000000 + np_y
+                print(p2)
+                assert(False)
+
+print("p2", p2 )
